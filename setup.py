2e0e2dbe2d7e9aa48a98798c5c6716654891d1c7
Antoine Guillaume <antoine.guillaume45@gmail.com>
2021-06-03 15:47:05 +0200

    setups file

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
 	download_url = 'https://github.com/baraline/cst/archive/v_01.tar.gz'
 	include_dirs=[numpy.get_include()],
     version="0.1",
+	keywords = ['shapelets', 'time-series-classification', 'shapelet-transform','convolutional-kernels']
 	url="https://github.com/baraline/CST",
     author_email="antoine.guillaume45@gmail.com",
 	python_requires='>=3.7',
@@ -34,31 +35,4 @@ setup(
         "numpy >= 1.18.5"
     ],
     zip_safe=False
-)
-
-setup(
-  name = 'cst',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
 )
