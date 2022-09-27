5b7a7797f4461d892330dee383728f76f23ad244
Antoine Guillaume <antoine.guillaume45@gmail.com>
2022-09-27 17:31:31 +0200

    missing imports

--- convst/__init__.py
+++ convst/__init__.py
@@ -2,4 +2,4 @@
 __author__ = 'Antoine Guillaume antoine.guillaume45@gmail.com'
 __version__ = "0.1.5.2"
 
-__all__ = ['transformers', 'classifiers', 'utils']
+__all__ = ['transformers', 'classifiers', 'utils', 'interpreters']
--- convst/interpreters/rdst_interpreter.py
+++ convst/interpreters/rdst_interpreter.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep 26 12:25:16 2022
-
-@author: antoi
 """
 
 # TODO: Define class taking as input a RDST transformer,
