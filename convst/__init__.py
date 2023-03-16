2fcab452455e5b7bd35f61e4494cb2c7b7e1d37e
Antoine Guillaume <antoine.guillaume45@gmail.com>
2023-03-16 18:52:32 +0100

    Updating version for pipy update

--- convst/__init__.py
+++ convst/__init__.py
@@ -1,6 +1,6 @@
 
 __author__ = 'Antoine Guillaume antoine.guillaume45@gmail.com'
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 
 __all__ = ['transformers', 'classifiers', 'utils', 'interpreters']
 
