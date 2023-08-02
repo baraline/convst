32eb93b4d3fb2166fa336d523242c54c05fa5342
Antoine Guillaume <antoine.guillaume45@gmail.com>
2023-08-02 10:37:16 +0200

    bump version in init

--- convst/__init__.py
+++ convst/__init__.py
@@ -1,6 +1,6 @@
 
 __author__ = 'Antoine Guillaume antoine.guillaume45@gmail.com'
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = ['transformers', 'classifiers', 'utils', 'interpreters']
 
